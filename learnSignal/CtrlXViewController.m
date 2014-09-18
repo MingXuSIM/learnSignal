80feb85f4e85ad8231ce85bad9a6afe661351bc4
Ming <mxu@systemsinmotion.com>
2014-09-18 09:32:10 -0400

    Initial Commit

---
+++ learnSignal/CtrlXViewController.m
@@ -0,0 +1,29 @@
+//
+//  CtrlXViewController.m
+//  learnSignal
+//
+//  Created by Ming Xu on 9/18/14.
+//  Copyright (c) 2014 body7. All rights reserved.
+//
+
+#import "CtrlXViewController.h"
+
+@interface CtrlXViewController ()
+
+@end
+
+@implementation CtrlXViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
